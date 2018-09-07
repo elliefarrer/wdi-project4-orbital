8307d7aaa8d36b7281dfdb9cad8f566541295dd8
Ellie Farrer <elliefarrermusic@gmail.com>
2018-09-07 16:53:18 +0100

    User can view their own profile page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
+// libraries
+import Auth from '../lib/Auth';
+
+// const userId = Auth.currentUserId();
+
 const Header = function() {
   return (
     <nav>
+      {Auth.isAuthenticated() &&
       <div className="column column-1of3 centered-text">
-        <Link to="/users/:userId">Profile</Link>
+        <Link to={`/users/${Auth.currentUserId()}`}>Profile</Link>
       </div>
+      }
       <div className="column column-2of3 centered-text">
         <Link to="/users">Index</Link>
       </div>
