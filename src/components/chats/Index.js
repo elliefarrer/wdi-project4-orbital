a7e4a6ca75a200475bfe7ae276a5b7b7dfee36ac
Ellie Farrer <elliefarrermusic@gmail.com>
2018-09-08 23:25:25 +0100

    Correct profile picture and first name come up on chats part of message index. Need to add most recent message

--- src/components/chats/Index.js
+++ src/components/chats/Index.js
@@ -3,52 +3,95 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 // import querystring from 'query-string';
 
+import _ from 'lodash';
+import moment from 'moment';
+
 // libraries
 import Auth from '../../lib/Auth';
 
 export default class ChatsIndex extends React.Component {
   state = {
-    matches: []
+    messaged: []
   };
 
+  getOtherUser = () => {
+    const currentUserId = Auth.currentUserId();
+    console.log('gou chats are', this.state.chats);
+    console.log('Logged in is', currentUserId);
+    if(this.state.chats) {
+      this.state.chats.forEach(chat => {
+        if(currentUserId === chat.userOne._id) {
+          this.state.messaged.push(chat.userTwo);
+        } else {
+          this.state.messaged.push(chat.userOne);
+        }
+        console.log('Messaged on state', this.state.messaged);
+      });
+    }
+  }
+
+  getMostRecentMessage = () => {
+    console.log('Get this to work');
+  }
+
+
+
   componentDidMount = () => {
-    axios.get(`/api/users/${Auth.currentUserId()}/swipes`, {
-      params: {
-        status: 'right'
-      }
-    })
+    axios.get(`/api/users/${Auth.currentUserId()}/swipes`)
       .then(res => this.setState({ swipes: res.data }));
 
     axios.get(`/api/users/${Auth.currentUserId()}/chats`)
       .then(res => this.setState({ chats: res.data }));
+
   }
 
   //TODO: write a function to get the correct user, whether it's userOne or userTwo. Then call it below.
 
   //TODO: write a function to get the most recent message and cut it to the first c.50 characters
 
-  //IDEA: add on users model, in swipes, a boolean of for mutual. Set it automatically to false. Then findById the swipee's swipes object. If our user is in there && status === right then change both of them to mutual=true. Else nothing. Then, on unmatch, findById the other person and change theirs to mutual=false. Or just delete their swipe too
 
-  //TODO: next get the swipes working on the index and show page. Use a lib file for this and import. Store left/right on event.target.value and stick that onto the body
 
   render() {
     console.log('Swipes are', this.state.swipes);
     console.log('Chats are', this.state.chats);
-    return (
-      <section>
-        {this.state.swipes && this.state.swipes.map((swipe, index) =>
-          <Link key={index} to={`/users/${swipe.userId._id}`}>
-            <img src={swipe.userId.profilePic} alt={swipe.userId.firstName} />
-            <p>{swipe.userId.firstName}</p>
-          </Link>
-        )}
 
-        {this.state.chats && this.state.chats.map(chat =>
-          <div key={chat._id}>
-            <img src={chat.userOne.profilePic} alt={chat.userOne.firstName} />
-            <p>{chat.userOne.firstName}</p>
+    //BUG: sometimes messages load for a second time, must be due to asynchronicity
+    const messagedUsers = this.getOtherUser(this.state.chats);
+    console.log('Messaged users are', messagedUsers);
+
+    console.log('Recent messages are', this.getMostRecentMessage());
+
+    return (
+      <section className="chats-index">
+        <div>
+          <h2>Matches</h2>
+          <div className="matches">
+            {this.state.swipes && this.state.swipes.map((swipe, index) =>
+              <div className="match" key={index} to={`/users/${swipe.userId._id}`}>
+                <img src={swipe.userId.profilePic} alt={swipe.userId.firstName} />
+                <Link to={`/users/${swipe.userId._id}`}>
+                  <i className="fas fa-info-circle"></i>
+                </Link>
+                <p>{swipe.userId.firstName}</p>
+              </div>
+            )}
           </div>
-        )}
+        </div>
+
+        <h2>Chats</h2>
+        <div className="chats-section">
+          {this.state.messaged && this.state.messaged.map(user =>
+            <div className="chat-container" key={user._id}>
+              <div className="column-1of2">
+                <img src={user.profilePic} alt={user.firstName} />
+              </div>
+              <div className="column-2of2">
+                <h3>{user.firstName}</h3>
+              </div>
+              <hr />
+            </div>
+          )}
+        </div>
       </section>
     );
   }
