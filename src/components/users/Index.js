7d9ae213d2d35c99ee3d3e13584427e0fedc1798
Ellie Farrer <elliefarrermusic@gmail.com>
2018-09-11 15:22:01 +0100

    Fixed error in uses index

--- src/components/users/Index.js
+++ src/components/users/Index.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 // Components
-import Location from './common/Location';
+// import Location from './common/Location';
 
 // libraries
 import Auth from '../../lib/Auth';
