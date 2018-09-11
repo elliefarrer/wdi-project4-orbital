8cb4be9b5115629320c72e5fea187383b5b3b004
Ellie Farrer <elliefarrermusic@gmail.com>
2018-09-11 15:12:50 +0100

    Updated index js

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect(dbURI);
 
-app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.append('Access-Control-Allow-Origin', ['*']);
+//   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.append('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
 app.use('/api', Router);
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
